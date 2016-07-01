84444033be5b2d8dd21f440b6750a3adc609a23c
Edward Jenkins <edward.jenkins@newcastle.ac.uk>
2016-07-01 09:44:50 +0100

    Update

--- en/home.php
+++ en/home.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'tagline' => 'Create your own courses, design them with friends and have Newcastle University Library help make them happen',
+    'tagline' => 'Create your own courses, design them with friends and have Newcastle University Library help make them happen.',
     'get_started' => 'Start your idea',
     'featured_ideas' => 'Featured Course Ideas',
 ];
@@ -10,7 +10,7 @@ return [
 
 What is EventMovement?
 
-EventMovement is a new and exciting way to create and commission training events and courses at Newcastle University Library. Start with an idea for an event, involve the community and make it happen! 
+EventMovement is a new and exciting way to create and commission training events and courses at Newcastle University Library. Start with an idea for an event, involve the community and make it happen!
 
 Each idea goes through three simple steps:
 
@@ -18,7 +18,7 @@ Each idea goes through three simple steps:
 Start an idea for a course with just a few clicks. Share it with your friends to get support for your idea.
 
 2.	Get Involved
-Work with other students to develop and shape the idea. Vote to have your voice heard and join in with the discussion. 
+Work with other students to develop and shape the idea. Vote to have your voice heard and join in with the discussion.
 
 3.	Make it Happen
 The library will respond to your idea with a blueprint for the course. Show the blueprint your support, or make your own blueprint for what the final course should be.

a352d54582aa16f44eeb4109c11ab5dc87ce191a
vagrant <vagrant@homestead>
2016-12-19 14:59:43 +0000

    Updates

--- en/home.php
+++ en/home.php
@@ -1,8 +1,8 @@
 <?php
 
 return array (
-  'tagline' => 'Help create technology to support people affected by dementia',
-  'get_started' => 'Share your idea',
+  'tagline' => 'Help to create technologies for people with dementia and their carers',
+  'get_started' => 'Share your ideas',
   'featured_ideas' => 'Featured Ideas',
   'be_inspired' => 'Be Inspired',
   'watch_the_video' => 'Watch the Video',
